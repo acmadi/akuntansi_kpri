c57d40bb58d858c47adc72d61d8e296ad1ac9dd2
Putu Widiarsa <nasi.be.guling@gmail.com>
2014-04-10 21:36:28 +0700

    initial commit

---
+++ akuntansi_app/admin/fTambahUser.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace akuntansi_app.admin
+{
+    partial class FTambahUser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUserId = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lvDaftarUser = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNama = new System.Windows.Forms.TextBox();
+            this.txtPass1 = new System.Windows.Forms.TextBox();
+            this.txtPass2 = new System.Windows.Forms.TextBox();
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(442, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "SIMPAN";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "User ID  : ";
+            // 
+            // txtUserId
+            // 
+            this.txtUserId.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUserId.Location = new System.Drawing.Point(87, 6);
+            this.txtUserId.Name = "txtUserId";
+            this.txtUserId.Size = new System.Drawing.Size(72, 25);
+            this.txtUserId.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lvDaftarUser);
+            this.groupBox1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 115);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(511, 215);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Daftar";
+            // 
+            // lvDaftarUser
+            // 
+            this.lvDaftarUser.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader4,
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.lvDaftarUser.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lvDaftarUser.GridLines = true;
+            this.lvDaftarUser.Location = new System.Drawing.Point(6, 19);
+            this.lvDaftarUser.Name = "lvDaftarUser";
+            this.lvDaftarUser.Size = new System.Drawing.Size(499, 185);
+            this.lvDaftarUser.TabIndex = 0;
+            this.lvDaftarUser.UseCompatibleStateImageBehavior = false;
+            this.lvDaftarUser.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "User ID";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Nama";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Password";
+            this.columnHeader3.Width = 121;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Nama  :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Password  :";
+            // 
+            // txtNama
+            // 
+            this.txtNama.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNama.Location = new System.Drawing.Point(87, 32);
+            this.txtNama.Name = "txtNama";
+            this.txtNama.Size = new System.Drawing.Size(245, 25);
+            this.txtNama.TabIndex = 6;
+            // 
+            // txtPass1
+            // 
+            this.txtPass1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPass1.Location = new System.Drawing.Point(87, 58);
+            this.txtPass1.Name = "txtPass1";
+            this.txtPass1.PasswordChar = '*';
+            this.txtPass1.Size = new System.Drawing.Size(245, 25);
+            this.txtPass1.TabIndex = 7;
+            // 
+            // txtPass2
+            // 
+            this.txtPass2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPass2.Location = new System.Drawing.Point(87, 84);
+            this.txtPass2.Name = "txtPass2";
+            this.txtPass2.PasswordChar = '*';
+            this.txtPass2.Size = new System.Drawing.Size(245, 25);
+            this.txtPass2.TabIndex = 8;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "No.";
+            // 
+            // FTambahUser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(538, 342);
+            this.Controls.Add(this.txtPass2);
+            this.Controls.Add(this.txtPass1);
+            this.Controls.Add(this.txtNama);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtUserId);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FTambahUser";
+            this.Text = "fTambahUser";
+            this.Load += new System.EventHandler(this.FTambahUser_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUserId;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListView lvDaftarUser;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNama;
+        private System.Windows.Forms.TextBox txtPass1;
+        private System.Windows.Forms.TextBox txtPass2;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+    }
+}
---
+++ akuntansi_app/admin/fTambahUser.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using _connectMySQL;
+using _tools;
+
+namespace akuntansi_app.admin
+{
+    public partial class FTambahUser : Form
+    {
+        #region KOMPONEN WAJIB
+        readonly CConnection _connect = new CConnection();
+        readonly CTools _tools = new CTools();
+        private MySqlConnection _connection;
+        private string _sqlQuery;
+        private readonly string _configurationManager = Properties.Settings.Default.Setting;
+        #endregion
+
+        public FTambahUser()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Control theControl = null; // the control which will be returned
+
+            string errMsg = "";
+            _connection = _connect.Connect(_configurationManager, ref errMsg, "123");
+
+            int maxid = _connect.GetMaxId(_connection, "tbl_user", "id") + 1;
+
+            _sqlQuery = "insert into db_toko.tMenu values (" + maxid + ", '" + txtNama.Text + "', '" + txtNama.Text +
+                        "', )";
+
+            if (!string.IsNullOrEmpty(errMsg))
+            {
+                MessageBox.Show(errMsg);
+                return;
+            }
+
+            if (_tools.CheckEmptyTextBox(this, ref theControl))
+            {
+                MessageBox.Show(@"Silahkan Lengkapi Data");
+                theControl.Focus();
+                return;
+            }
+            try
+            {
+                _connect.Insertion(_sqlQuery
+                    , _connection);
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(@"Error performing insert to database : " + ex.Message);
+                _connection.Close();
+                return;
+            }
+            _connection.Close();
+            ShowUser();
+            _tools.ClearControlText(this);
+        }
+
+        private void FTambahUser_Load(object sender, EventArgs e)
+        {
+            ShowUser();
+        }
+
+        private void ShowUser()
+        {
+            List<ListViewItem> groupItems = new List<ListViewItem>();
+            string errMsg = "";
+
+            _connection = _connect.Connect(_configurationManager, ref errMsg, "123");
+
+            MySqlDataReader reader = _connect.Reading("SELECT a.user, a.nama , a.pass " +
+                "tbl_user a", _connection);
+
+            if (!string.IsNullOrEmpty(errMsg))
+            {
+                MessageBox.Show(errMsg);
+                return;
+            }
+
+            int noUrut = 1;
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    ListViewItem item = new ListViewItem(Convert.ToString(noUrut));
+                    item.SubItems.Add(Convert.ToString(reader[0]));
+                    item.SubItems.Add(Convert.ToString(reader[1]));
+                    item.SubItems.Add(Convert.ToString(reader[2]));
+                    groupItems.Add(item);
+                    noUrut++;
+                }
+                reader.Close();
+            }
+            lvDaftarUser.BeginUpdate();
+            lvDaftarUser.Items.Clear();
+            lvDaftarUser.Items.AddRange(groupItems.ToArray());
+            lvDaftarUser.EndUpdate();
+            groupItems.Clear();
+
+            _tools.AutoResizeListView(lvDaftarUser, true);
+            _connection.Close();
+        }
+    }
+}
